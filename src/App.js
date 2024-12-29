3c95cf513d887839981e76267f7ea369f459cb3f
annegleichmann <anne.gleichmann@web.de>
2024-12-29 10:44:58 +0100

    Added file + axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+import CurrentWeather from "./CurrentWeather";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <h1>Weather App</h1>
+      <CurrentWeather />
     </div>
   );
 }
-
-export default App;
---
+++ src/CurrentWeather.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=96771e971243152d6b8948878c26adde&units=metric`;
+    axios.get(apiURL).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city" onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind Speed: {weather.wind} km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />{" "}
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return <div>{form}</div>;
+  }
+}
