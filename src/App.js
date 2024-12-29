4f17ffecb627eeda11459999477652f5dfd5bbed
annegleichmann <anne.gleichmann@web.de>
2024-12-29 11:28:11 +0100

    added links to Footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <CurrentWeather />
+      <br />
+      <p>
+        This project was coded by Anne Gleichmann and it is{" "}
+        <a
+          href="https://github.com/annegleichmann/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+      </p>
     </div>
   );
 }
