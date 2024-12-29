e4c313428a35ca72b5387f8b70ab9e260eba50b6
annegleichmann <anne.gleichmann@web.de>
2024-12-29 11:07:43 +0100

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      This project was coded by{" "}
+      <a
+        href="https://github.com/annegleichmann"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Anne Gleichmann
+      </a>{" "}
+      and is open-sourced on{" "}
+      <a
+        href="https://github.com/annegleichmann/weather-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub
+      </a>{" "}
+      and hosted on{" "}
+      <a
+        href="https://weather-react-app-shecodes-homework.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Netlify
+      </a>
+      .
+    </div>
+  );
+}
